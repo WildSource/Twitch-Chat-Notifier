057b5eaf1fb0476442df40fb997eff1c862734e8
WildSource <ilovetrap123@hotmail.com>
2025-09-09 12:51:29 -0400

    Add open-browser dependency

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Web.Scotty
 import Web.Scotty.TLS
 
 main :: IO ()
-main = scottyTLS 3000 "private.key" "certificate.crt" $ do
-  get "/auth" $ do
-    json ("Hello, World !" :: String)
+main = do
+  scottyTLS 3000 "private.key" "certificate.crt" $ do
+    get "/auth" $ do
+      json ("Hello, World !" :: String)
