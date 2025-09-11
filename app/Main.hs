b48c86a9a30360a5054d5b8533753654c99ca9fd
WildSource <ilovetrap123@hotmail.com>
2025-09-10 22:11:35 -0400

    Run threaded option and make threads

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Concurrent
+import Control.Monad
+import Web.Browser
 import Web.Scotty
 import Web.Scotty.TLS
 
+authenticationEndpoint :: IO ()
+authenticationEndpoint = do
+    scottyTLS 3000 "private.key" "certificate.crt" $ do
+      get "/auth" $ do
+        json ("Hello, World !" :: String)
+      
 main :: IO ()
 main = do
-  scottyTLS 3000 "private.key" "certificate.crt" $ do
-    get "/auth" $ do
-      json ("Hello, World !" :: String)
+  thread1 <- forkIO authenticationEndpoint
+  thread2 <- forkIO $ void $ openBrowser "https://id.twitch.tv/oauth2/authorize"
+  pure ()
+
